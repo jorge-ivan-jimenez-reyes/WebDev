ab775528b11fe506a8e31f79531e91006d54062d
jorge-ivan-jimenez-reyes <0251139@up.edu.mx>
2024-08-25 20:03:02 -0600

    added scripts.js

---
+++ website1/js/script.js
@@ -0,0 +1,9 @@
+// script.js
+
+console.log("Página cargada correctamente");
+
+// Aquí puedes añadir más funcionalidades interactivas usando JavaScript
+
+window.addEventListener('load', function() {
+    console.log("Todos los recursos de la página han sido cargados.");
+});
